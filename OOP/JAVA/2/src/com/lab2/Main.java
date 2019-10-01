137aa4eaf46ac7734301f3055321151e846a8f0b
alanlatte <shukurovfoteh@gmail.com>
2019-10-02 00:47:57 +0300

    [+] Lab 2b already done

---
+++ OOP/JAVA/2/src/com/lab2/Main.java
@@ -0,0 +1,49 @@
+package com.lab2;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class Main {
+    public static void main(String[] args) {
+        List<Integer> data = new ArrayList<>();
+        List<Integer> resultArray = new ArrayList<>();
+        Scanner readLine = new Scanner(System.in);
+        while (true){
+            System.out.print("Write some number: ");
+            int readData = readLine.nextInt();
+            if ( readData == 0 ){
+                break;
+            }else{
+                data.add(readData);
+            }
+        }
+        for (int number :
+                data) {
+            resultArray.add(calculate(number));
+        }
+        System.out.println(resultArray);
+    }
+    static Integer calculate(int number){
+        List<Integer> resultArray = new ArrayList<>();
+        while (number > 1){
+            int divider = 2;
+            while (true){
+                if (number % divider == 0){
+                    number /= divider;
+                    resultArray.add(divider);
+                    break;
+                }else{
+                    divider ++;
+                }
+            }
+        }
+        AtomicInteger resultValue = new AtomicInteger();
+        for (int i :
+                resultArray) {
+            resultValue.addAndGet(i);
+        }
+        return resultValue.get();
+    }
+}
